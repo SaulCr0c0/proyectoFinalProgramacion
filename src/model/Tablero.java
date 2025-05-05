6aefc3c3301fb2d14d0a58b37885ed8653861498
Saul <saulcordovavill@gmail.com>
2025-05-05 15:20:13 -0400

    fake commit

---
+++ src/model/Tablero.java
@@ -0,0 +1,21 @@
+
+package model;
+
+public class Tablero {
+    private char mTablero[][];
+    private static int iTamanio = 9;
+    
+    public Tablero(){
+        mTablero = new char[iTamanio][iTamanio];
+    }
+    public void generarTabla(){
+        for(int i=0; i<9; i++){
+            for(int j = 0; j<9;j++){
+                if((i<6 && i>2)){
+                    mTablero[][] = 
+                }
+            }
+        }
+        mTablero[4][4] = ' ';
+    }
+}
